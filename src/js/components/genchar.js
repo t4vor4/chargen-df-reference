eb309642966455d1387d3ec8064f40eac8a5e4f4
Fernando <fernando@agenciaeplus.com.br>
2020-06-27 15:02:35 -0300

    [FIX] Change chance of appear elements in character roll. Added genre in charachter description

--- src/js/components/genchar.js
+++ src/js/components/genchar.js
@@ -15,58 +15,73 @@ export default class GenChar {
         return obj[resultado];
     }
 
-    montaChar() {
-        
-        const $p = {}
-        
-        const $i = this.roll(Char.idade);
-        
-        const $iAtual = Math.floor(Math.random() * ($i.max - $i.min) ) + $i.min;
+    genero() {
+        return this.roll(Char.sexo);
+    }
 
-        let $pFisico = _ => {
-            let $arr = Char.portefisico1;
-            if ($i.nome === 'criança'){
-                for (const i in $arr) {
-                    $arr[i] === "muito alto" && $arr.splice(i,1)
-                }
-            }
-            return $arr;
+    idade() {
+        const idade = this.roll(Char.idade);
+        return `entre ${idade.min} e ${idade.max} anos`
+    }
+
+    fisico() {
+        const $roll = this.roll;
+        const body = {
+            a: $roll(Char.portefisico1),
+            b: $roll(Char.portefisico2)
         }
-        
-        const pFisico1 = this.roll($pFisico());
+        return body;
+    }
 
-        const $c = this.roll(Char.cabelo1);
-        
+    cabelo() {
+        const type1 = this.roll(Char.cabelo1);
+        const type2 = type1 === 'careca' ? 'não tem' : this.roll(Char.cabelo2);
+        return {a: type1, b: type2}
+    }
 
-        $p.genero = this.roll(Char.sexo);
-        $p.idade1 = $i.nome;
-        $p.idade2 = `${$iAtual} anos`;
-        $p.body1 = pFisico1;
-        $p.body2 = this.roll(Char.portefisico2);
-        $p.cabelo1 = $c;
-        $p.cabelo2 = $c !== 'careca' ? this.roll(Char.cabelo2) : null;
-        $p.humor = this.roll(Char.humor);
-        $p.persona = `O personagem é um ${this.roll(Char.list_a)} ${this.roll(Char.list_b)}`
+    humor() {
+        return this.roll(Char.humor);
+    }
+
+    personaA() {
+        return  this.roll(Char.list_a);
+    }
+
+    personaB() {
+        return  this.roll(Char.list_b);
+    }
+
+    montaChar() {
+
+        const $p = {}
+
+        $p.genero = this.genero();
+        $p.idade = this.idade();
+        $p.body = this.fisico();
+        $p.cabelo = this.cabelo();
+        $p.humor = this.humor();
+        $p.personaA = this.personaA();
+        $p.personaB = this.personaB();
+        $p.validador = $p.genero === 'masculino'? 'a' : 'b';
+        $p.personaC = `O personagem é ${$p.personaA[$p.validador]} ${$p.personaB}`
 
         return $p;
 
     }
 
     montaHtml(obj) {
-        const {genero, idade1, idade2, body1, body2, cabelo1, cabelo2, humor, persona} = obj;
-        
         const $html = `
         <section>
         <ul>
-            <li gp-genero><strong>Sexo:</strong> ${genero};</li>
-            <li gp-idade1><strong>Idade 1:</strong> ${idade1};</li>
-            <li gp-idade2><strong>Idade 2:</strong> ${idade2};</li>
-            <li gp-portefisico1><strong>Porte Físico 1:</strong> ${body1};</li>
-            <li gp-portefisico2><strong>Porte Físico 2:</strong> ${body2};</li>
-            <li gp-cabelo1><strong>Cabelo:</strong> ${cabelo1};</li>
-            ${!!cabelo2 ? `<li gp-cabelo2><strong>Cabelo:</strong> ${cabelo2};</li>` : ''}
-            <li gp-humor><strong>Humor:</strong> ${humor};</li>
-            <li gp-persona><strong>Persona:</strong> ${persona}</li>
+            <li gp-genero><strong>Sexo:</strong> ${obj.genero};</li>
+            <li gp-idade1><strong>Idade:</strong> ${obj.idade};</li>
+            <li gp-portefisico1><strong>Porte Físico 1:</strong> ${obj.body.a};</li>
+            <li gp-portefisico2><strong>Porte Físico 2:</strong> ${obj.body.b};</li>
+            <li gp-cabelo1><strong>Cabelo (tamanho):</strong> ${obj.cabelo.a};</li>
+            <li gp-cabelo2><strong>Cabelo (tipo):</strong> ${obj.cabelo.b};</li>
+            <li gp-humor><strong>Humor:</strong> ${obj.humor};</li>
+            <li gp-persona><strong>Persona:</strong> ${obj.personaC}</li>
+            
         </ul>
         <button gp-button class="gp_button">Gerar um novo personagem</button>
         </section>
